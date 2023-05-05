54dd3f4d9e7e1a5bca4ae3d04edd5517f11cb9fb
Axel Sotelo <33433481+axew26@users.noreply.github.com>
2023-05-04 22:03:10 -0500

    003 - base tarjeta investigador

---
+++ src/components/tarjeta-investigador/tarjeta-investigador.tsx
@@ -0,0 +1,16 @@
+import './tarjeta-investigador.css';
+
+export default function TarjetaInvestigador() {
+    return (
+        <div className='tarjeta-investigador w-[305px] h-[483px] rounded-lg text-[0.8125rem] text-slate-900 shadow-xl shadow-black/5 ring-1 ring-slate-700/10'>
+            <div className='h-[50%] contenedor-imagen'>
+                <img className="foto-investigador" src="/mock_investigador.png" alt="mock" />
+            </div>
+            <div className='h-[50%] contenedor-informacion p-3'>
+                <h2 className='text-2xl'>ana isabel oviedo carrascal, phd</h2>
+                <p className='mt-4 text-base leading-7'>Investigadora Principal UPB</p>
+                <p className='text-base leading-7'>ana-oviedo@upb.edu.co</p>
+            </div>
+        </div>
+    );
+  }
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,15 @@
+import TarjetaInvestigador from "../components/tarjeta-investigador/tarjeta-investigador";
+
 export default function Root() {
-    return (
-      <>
-       <div><p>Prueba de que react router funciona</p></div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div>
+        <p>Prueba de que react router funciona</p>
+      </div>
+      <div className="cardTest">
+      <TarjetaInvestigador></TarjetaInvestigador>
+      <TarjetaInvestigador></TarjetaInvestigador>
+      </div>
+    </>
+  );
+}
