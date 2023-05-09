b046a48f523914588fd410feb2727d1ed6fa105d
FelipeFranco97 <48889338+FelipeFranco97@users.noreply.github.com>
2023-05-09 16:32:01 -0500

    006 - css y logo provisional

--- src/components/card-proyecto/card-proyecto.tsx
+++ src/components/card-proyecto/card-proyecto.tsx
@@ -5,14 +5,21 @@ export default function CardProyecto() {
     return(
         <div className='card-proyecto'>
             <div className='contenedor-informacion'>
-                <h1 className='text-8xl'>1</h1>
-                <h3 className='text-xl'>Analítica Descriptiva y Predictiva del Proceso Productivo de Cemento y Concreto</h3>
-                <p className='text-base'>Objetivo:  Estudiar el proceso productivo de cemento y concreto en la empresa Cementos Argos, mediante técnicas de aprendizaje estadístico aplicadas a los datos históricos, para el descubrimiento de patrones y variables relevantes que permitan identificar las combinaciones adecuadas que producen los valores nominales con menor dispersión.</p>
+                <h1 className='numero-card'>1</h1>
+                <h3 className='titulo-card'>Analítica Descriptiva y Predictiva del Proceso Productivo de Cemento y Concreto</h3>
+                <br />
+                <p>
+                    <strong>Objetivo:</strong> Estudiar el proceso productivo de cemento y concreto en la empresa Cementos Argos, mediante técnicas de aprendizaje estadístico aplicadas a los datos históricos, para el descubrimiento de patrones y variables relevantes que permitan identificar las combinaciones adecuadas que producen los valores nominales con menor dispersión.
+                </p>
             </div>
             <hr />
-            <div className='contenedor-lider grid grid-cols-2'>
-                <h3 className='text-xl'>Lidera</h3>
-                <img className='' src={eafitLogo} alt="" />
+            <div className='contenedor-lider'>
+                <h3>
+                    <strong>Lidera:</strong>
+                </h3>
+                <a href="https://www.eafit.edu.co" target="_blank">
+                    <img className='' src={eafitLogo} alt="" />
+                </a>
             </div>
         </div>
     );
