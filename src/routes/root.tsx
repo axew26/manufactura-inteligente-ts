e675a235287096480f6af62caf4ffc8ae6671939
Axel Sotelo <33433481+axew26@users.noreply.github.com>
2023-05-13 13:34:32 -0500

    020 - POC librerias swper y splide

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,76 @@
+import "./root.scss";
+
+/**
+ * Prueba de librerias para carrusel
+ */
+/**
+ * Splide
+ */
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+
+/**
+ * Swiper
+ */
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+
 export default function Root() {
-    return (
-      <>
-       <div><p>Prueba de que react router funciona</p></div>
-      </>
-    );
-  }
+  const imagenes = [...new Array(10)].map((_, index: number) => (
+    <img key={index} src="/vite.svg" alt="Image 1" />
+  ));
+
+  /**
+   * Splide
+   */
+  const splideSlides = imagenes.map((element: JSX.Element, index: number) => (
+    <SplideSlide key={index}>{element}</SplideSlide>
+  ));
+
+  /**
+   * Swiper
+   */
+  const swiperSlides = imagenes.map((element: JSX.Element, index: number) => (
+    <SwiperSlide key={index}>{element}</SwiperSlide>
+  ));
+
+  const gridImagenes = <div className="grid-imagenes">
+      {imagenes}
+  </div>;
+
+  return (
+    <>
+      {gridImagenes}
+      <section>
+        <h2>SPLIDE</h2>
+        <Splide
+          options={{
+            padding: "5rem",
+            perPage: 1,
+            perMove: 1,
+            arrows: false,
+          }}
+        >
+          {splideSlides}
+        </Splide>
+      </section>
+
+      <section>
+        <h2>Swiper</h2>
+        <Swiper
+          slidesPerView={1.5}
+          spaceBetween={0}
+          centeredSlides={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+        >
+          {swiperSlides}
+        </Swiper>
+      </section>
+    </>
+  );
+}
