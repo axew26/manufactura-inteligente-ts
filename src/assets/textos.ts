6939fa051f25a7df335c95095102b2eca79f4749
FelipeFranco97 <48889338+FelipeFranco97@users.noreply.github.com>
2023-05-14 15:21:11 -0500

    022 - Logos agregados y pequeña correción en CSS

--- src/assets/textos.ts
+++ src/assets/textos.ts
@@ -5,21 +5,21 @@ export const proyectos: Proyecto[] = [
         numero:"1",
         titulo:"Analítica Descriptiva y Predictiva del Proceso Productivo de Cemento y Concreto",
         descripcion:"Objetivo: Estudiar el proceso productivo de cemento y concreto en la empresa Cementos Argos, mediante técnicas de aprendizaje estadístico aplicadas a los datos históricos, para el descubrimiento de patrones y variables relevantes que permitan identificar las combinaciones adecuadas que producen los valores nominales con menor dispersión.",
-        urlImagen:"/Logo_EAFIT.png",
+        urlImagen:"/Logo_EAFIT.svg",
         urlSitio:"https://www.eafit.edu.co"
     },
     {
         numero:"2",
         titulo:"Aplicación de la Tecnología IoT en la Producción de Concreto",
         descripcion:"Objetivo: Diseñar una infraestructura de sensado, mediante la aplicación de IoT, para el monitoreo del proceso de producción del concreto.",
-        urlImagen:"/Logo_UPB.png",
+        urlImagen:"/Logo_UPB.svg",
         urlSitio:"https://www.upb.edu.co/es/home"
     },
     {
         numero:"3",
         titulo:"Manufactura Inteligente de Cemento y Concreto",
         descripcion:"Objetivo: Desarrollar modelos predictivos para la formulación de concretos, usando técnicas de Inteligencia Artificial, para la reducción de la incertidumbre en las principales propiedades de desempeño del concreto (fraguados, resistencias, asentamiento).",
-        urlImagen:"/Logo_UPB.png",
+        urlImagen:"/Logo_UPB.svg",
         urlSitio:"https://www.upb.edu.co/es/home"
     }
 ]
