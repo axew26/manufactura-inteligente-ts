fa21b21436e2458747ed144e62314aa6b91fc6c5
FelipeFranco97 <48889338+FelipeFranco97@users.noreply.github.com>
2023-05-14 11:55:13 -0500

    006 - PR Comentarios

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,21 +1,7 @@
-import CardProyecto from "../components/card-proyecto/card-proyecto";
-import { cards } from "../assets/textos";
-import { Card } from "../types/card-proyecto.type";
-
 export default function Root() {
-  const cardView = cards.map(
-    (card: Card, key:number) =>(
-      <CardProyecto
-        key={key}
-        card={card}
-      ></CardProyecto>
-    )
-  )
-  return (
-    <>
-      <div className="cardComponent">
-        {cardView}
-      </div>
-    </>
-  );
-}
+    return (
+      <>
+       <div><p>Prueba de que react router funciona</p></div>
+      </>
+    );
+  }
