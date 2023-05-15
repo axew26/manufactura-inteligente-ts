48314947b147615a0051758c484adaa93a4ac203
FelipeFranco97 <48889338+FelipeFranco97@users.noreply.github.com>
2023-05-15 17:01:23 -0500

    011 - Footer con comportamiento dinámico

--- src/assets/textos.ts
+++ src/assets/textos.ts
@@ -1,5 +1,6 @@
 import { Investigador } from "../types/tarjeta-investigador.type";
 import { Proyecto } from "../types/proyecto.type";
+import {EntidadesFooter } from "../types/footer.type";
 
 export const investigadores: Investigador[] = [
   {
@@ -36,18 +37,18 @@ export const investigadores: Investigador[] = [
     urlImagen: "/mock_investigador.png",
     nombre: "NATHALIA ANDREA ORTEGA",
     titulos: [
-        {
-            nombre: 'Ingeniera de Sistemas',
-            resaltar: false,
-        },
-        {
-            nombre: 'Ingeniera en Ciencia de Datos',
-            resaltar: false,
-        },
-        {
-            nombre: 'Joven Investigadora',
-            resaltar: true,
-        },
+      {
+        nombre: "Ingeniera de Sistemas",
+        resaltar: false,
+      },
+      {
+        nombre: "Ingeniera en Ciencia de Datos",
+        resaltar: false,
+      },
+      {
+        nombre: "Joven Investigadora",
+        resaltar: true,
+      },
     ],
     contacto: "nathalia.ortega@upb.edu.co",
   },
@@ -55,18 +56,18 @@ export const investigadores: Investigador[] = [
     urlImagen: "/mock_investigador.png",
     nombre: "CAROLINA ZULUAGA ÁLVAREZ",
     titulos: [
-        {
-            nombre: 'Ingeniera Eléctrica',
-            resaltar: false,
-        },
-        {
-            nombre: 'Ingeniera Eléctronica',
-            resaltar: false,
-        },
-        {
-            nombre: 'Joven Investigadora',
-            resaltar: true,
-        },
+      {
+        nombre: "Ingeniera Eléctrica",
+        resaltar: false,
+      },
+      {
+        nombre: "Ingeniera Eléctronica",
+        resaltar: false,
+      },
+      {
+        nombre: "Joven Investigadora",
+        resaltar: true,
+      },
     ],
     contacto: "carolina.zuluagaa@upb.edu.co",
   },
@@ -74,39 +75,54 @@ export const investigadores: Investigador[] = [
     urlImagen: "/mock_investigador.png",
     nombre: "ESTEBANA OROZCO RAMÍREZ",
     titulos: [
-        {
-            nombre: 'Ingeniera de Telecomunicaciones',
-            resaltar: false,
-        },
-        {
-            nombre: 'Estudiante de Maestría',
-            resaltar: true,
-        },
+      {
+        nombre: "Ingeniera de Telecomunicaciones",
+        resaltar: false,
+      },
+      {
+        nombre: "Estudiante de Maestría",
+        resaltar: true,
+      },
     ],
     contacto: "estebana.orozco@upb.edu.co",
   },
 ];
 
 export const proyectos: Proyecto[] = [
-    {
-        numero:"1",
-        titulo:"Analítica Descriptiva y Predictiva del Proceso Productivo de Cemento y Concreto",
-        descripcion:"Objetivo: Estudiar el proceso productivo de cemento y concreto en la empresa Cementos Argos, mediante técnicas de aprendizaje estadístico aplicadas a los datos históricos, para el descubrimiento de patrones y variables relevantes que permitan identificar las combinaciones adecuadas que producen los valores nominales con menor dispersión.",
-        urlImagen:"/Logo_EAFIT.png",
-        urlSitio:"https://www.eafit.edu.co"
-    },
-    {
-        numero:"2",
-        titulo:"Aplicación de la Tecnología IoT en la Producción de Concreto",
-        descripcion:"Objetivo: Diseñar una infraestructura de sensado, mediante la aplicación de IoT, para el monitoreo del proceso de producción del concreto.",
-        urlImagen:"/Logo_UPB.png",
-        urlSitio:"https://www.upb.edu.co/es/home"
-    },
-    {
-        numero:"3",
-        titulo:"Manufactura Inteligente de Cemento y Concreto",
-        descripcion:"Objetivo: Desarrollar modelos predictivos para la formulación de concretos, usando técnicas de Inteligencia Artificial, para la reducción de la incertidumbre en las principales propiedades de desempeño del concreto (fraguados, resistencias, asentamiento).",
-        urlImagen:"/Logo_UPB.png",
-        urlSitio:"https://www.upb.edu.co/es/home"
-    }
-]
+  {
+    numero: "1",
+    titulo:
+      "Analítica Descriptiva y Predictiva del Proceso Productivo de Cemento y Concreto",
+    descripcion:
+      "Objetivo: Estudiar el proceso productivo de cemento y concreto en la empresa Cementos Argos, mediante técnicas de aprendizaje estadístico aplicadas a los datos históricos, para el descubrimiento de patrones y variables relevantes que permitan identificar las combinaciones adecuadas que producen los valores nominales con menor dispersión.",
+    urlImagen: "/Logo_EAFIT.png",
+    urlSitio: "https://www.eafit.edu.co",
+  },
+  {
+    numero: "2",
+    titulo: "Aplicación de la Tecnología IoT en la Producción de Concreto",
+    descripcion:
+      "Objetivo: Diseñar una infraestructura de sensado, mediante la aplicación de IoT, para el monitoreo del proceso de producción del concreto.",
+    urlImagen: "/Logo_UPB.png",
+    urlSitio: "https://www.upb.edu.co/es/home",
+  },
+  {
+    numero: "3",
+    titulo: "Manufactura Inteligente de Cemento y Concreto",
+    descripcion:
+      "Objetivo: Desarrollar modelos predictivos para la formulación de concretos, usando técnicas de Inteligencia Artificial, para la reducción de la incertidumbre en las principales propiedades de desempeño del concreto (fraguados, resistencias, asentamiento).",
+    urlImagen: "/Logo_UPB.png",
+    urlSitio: "https://www.upb.edu.co/es/home",
+  },
+];
+
+export const entidades: EntidadesFooter[] = [
+  {
+    urlImagenArgos: "/Logo_Argos.svg",
+    urlSitioArgos: "https://argos.co/",
+    urlImagenUPB: "/Logo_UPB.svg",
+    urlSitioUPB: "https://www.upb.edu.co/es/home",
+    urlImagenMinciencias: "/Logo_Minciencias.svg",
+    urlSitioMinciencias: "https://minciencias.gov.co/",
+  },
+];
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,29 @@
+import "./footer.scss";
+import { EntidadesFooter } from "../../types/footer.type";
+
+export default function Footer(props: Record<string, unknown>) {
+  const footer = props.footer as EntidadesFooter;
+
+  return (
+    <footer className="footer-container">
+      <div className="entidades-container">
+        <a href={footer.urlSitioArgos} target="_blank">
+          <img src={footer.urlImagenArgos} alt="" className="argos-footer" />
+        </a>
+        <hr />
+        <a href={footer.urlSitioUPB} target="_blank">
+          <img src={footer.urlImagenUPB} alt="" className="upb-footer" />
+        </a>
+        <hr />
+        <a href={footer.urlSitioMinciencias} target="_blank">
+          <img
+            src={footer.urlImagenMinciencias}
+            alt=""
+            className="minciencias-footer"
+          />
+        </a>
+      </div>
+      <div className="recuadro-footer"></div>
+    </footer>
+  );
+}
