8d3a51c68a0bd3bca718fe9b7b13e06ec867eb35
Axel Sotelo <33433481+axew26@users.noreply.github.com>
2023-05-14 20:40:15 -0500

    010 - Ajuste tarjeta investigador

--- src/components/tarjeta-investigador/tarjeta-investigador.tsx
+++ src/components/tarjeta-investigador/tarjeta-investigador.tsx
@@ -6,16 +6,19 @@ export default function TarjetaInvestigador(props:Record<string,unknown>) {
 
     let titulosView:JSX.Element;
 
+    let listaTitulos = false;
+
     if(typeof investigador.titulos === 'string') {
-        titulosView = <p className='mt-2 text-base leading-7'>{investigador.titulos}</p>;
+        titulosView = <p className='mt-2 text-base leading-7 titulo'>{investigador.titulos}</p>;
     }
     else {
+        listaTitulos = true;
         titulosView = <ul>
             {
                 investigador.titulos.map((titulo:Titulo, key:number)=>{
                     if(titulo.resaltar) {
                         return <li key={key}>
-                            <strong>{titulo.nombre}</strong>
+                            <strong className="titulo-resaltado">{titulo.nombre}</strong>
                         </li>
                     } else {
                         return <li key={key}>{titulo.nombre}</li>
@@ -25,8 +28,9 @@ export default function TarjetaInvestigador(props:Record<string,unknown>) {
             }
         </ul>
     }
+
     return (
-        <div className='tarjeta-investigador w-[305px] h-[483px] rounded-lg text-[0.8125rem] text-slate-900 shadow-xl shadow-black/5 ring-1 ring-slate-700/10'>
+        <div className='tarjeta-investigador w-[305px] h-[483px] text-[0.8125rem] '>
             <div className='h-[50%] contenedor-imagen'>
                 <img className="foto-investigador" src={investigador.urlImagen} alt="mock" />
             </div>
@@ -34,7 +38,9 @@ export default function TarjetaInvestigador(props:Record<string,unknown>) {
             <div className='h-[50%] contenedor-informacion p-3'>
                 <h2 className='text-2xl'>{investigador.nombre}</h2>
                 {titulosView}
-                <p className='text-base leading-7'>{investigador.contacto}</p>
+                <div className={listaTitulos?'contacto flex flex-col justify-end h-1/3':'contacto'}>
+                    <p className='text-base leading-7'>{investigador.contacto}</p>
+                </div>
             </div>
         </div>
     );
