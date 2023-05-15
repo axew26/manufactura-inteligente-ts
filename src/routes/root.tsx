b2250eaf2654052863506aa506fe58affe1f3bd5
Axel Sotelo <33433481+axew26@users.noreply.github.com>
2023-05-14 20:42:35 -0500

    010 - PR ajustes

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,22 +1,7 @@
-import TarjetaInvestigador from "../components/tarjeta-investigador/tarjeta-investigador";
-import { investigadores } from "../assets/textos";
-import { Investigador } from "../types/tarjeta-investigador.type";
-
 export default function Root() {
-  const investigadoresView = investigadores.map(
-    (investigador: Investigador, key:number) => (
-      <TarjetaInvestigador
-        key={key}
-        investigador={investigador}
-      ></TarjetaInvestigador>
-    )
-  );
-
-  return (
-    <>
-      <div className="cardTest">
-        {investigadoresView}
-      </div>
-    </>
-  );
-}
+    return (
+      <>
+       <div><p>Prueba de que react router funciona</p></div>
+      </>
+    );
+  }
